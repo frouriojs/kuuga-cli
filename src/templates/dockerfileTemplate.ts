199169bb7759b67d4ef4a6e5134bc196517a5c49
solufa <solufa2020@gmail.com>
2025-06-26 19:34:54 +0900

    fix: update dockerfileTemplate

--- src/templates/dockerfileTemplate.ts
+++ src/templates/dockerfileTemplate.ts
@@ -10,7 +10,7 @@ const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8')) as Rec
 export const dockerfileTemplate = `FROM node:22-slim
 
 RUN apt-get update && \\
-    apt-get install -y curl unzip jq git && \\
+    apt-get install -y curl unzip jq git cron && \\
     curl -L -o kubo.tar.gz https://dist.ipfs.io/kubo/v0.25.0/kubo_v0.25.0_linux-amd64.tar.gz && \\
     tar -xzf kubo.tar.gz && \\
     cd kubo && \\
@@ -22,11 +22,14 @@ RUN apt-get update && \\
 
 WORKDIR /app
 
+ARG KUUGA_KEY
+
 COPY . .
+RUN echo $KUUGA_KEY
 RUN ipfs init
 RUN npm i -g kuuga-cli@${packageJson.version}
 
 EXPOSE 4001
 
-CMD sh -c "ipfs daemon & sleep 5 && kuuga pin"
+CMD sh -c "ipfs daemon & sleep 10 && kuuga pin && while true; do sleep 3600; kuuga pin; done"
 `;
